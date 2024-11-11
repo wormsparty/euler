dfe10922bd5961664f86ab98ec68a2d4045ac569
Marc-Olivier <wormsparty@gmail.com>
2024-11-11 20:45:47 +0100

    Pass column mapping, which is a lot easier

--- src/filter.rs
+++ src/filter.rs
@@ -1,8 +1,9 @@
 use std::collections::HashMap;
 use std::string::String;
-use sea_orm::{ColumnTrait, DbConn, Iterable, Order, QueryFilter, QueryOrder, QuerySelect};
+use sea_orm::{ColumnTrait, DbConn, Order, QueryFilter, QueryOrder, QuerySelect};
 use sea_orm::{DbErr, EntityTrait};
 use serde::Deserialize;
+use crate::entities::entity::Column;
 
 #[derive(Deserialize)]
 pub struct Sort {
@@ -31,34 +32,17 @@ pub struct Query {
 }
 
 impl Query {
-    pub async fn get_column_by_name<E: EntityTrait>(
-        &self,
-        col_name: &str
-    ) -> Result<<E as EntityTrait>::Column, DbErr> {
-        // TODO: How to we optimize this ?!
-        E::Column::iter()
-            .find(|col| format!("{:?}", col) == col_name)
-            .ok_or_else(|| {
-                DbErr::Custom(format!(
-                    "Colonne '{}' non trouvée. Les colonnes disponibles sont: {:?}",
-                    col_name,
-                    E::Column::iter()
-                        .map(|c| format!("{:?}", c))
-                        .collect::<Vec<_>>()
-                ))
-            })
-    }
-
     pub async fn apply_filters<E: EntityTrait>(
         &self,
         global_searchable_fields: &Vec<&str>,
+        column_map: &HashMap<&str, Column>,
         db: &DbConn,
     ) -> Result<Vec<E::Model>, DbErr> {
         let mut qs = E::find();
 
         // Filter by each field
         for (name, filter) in &self.filter {
-            let column= self.get_column_by_name::<E>(&name).await?;
+            let column= column_map.get(name.as_str()).unwrap();
 
             if filter.filter_type != "text" {
                 return Err(DbErr::Custom("Unsupported filter type".to_string()));
@@ -84,17 +68,17 @@ impl Query {
         if !self.global_search.is_empty() {
             // This seems to be safe from injection as the builder replaces spaces with underscores
             for field in global_searchable_fields {
-                let column = self.get_column_by_name::<E>(&field).await?;
+                let column= column_map.get(field).unwrap();
                 qs = qs.filter(column.contains(&self.global_search));
             }
         }
 
         // Sorting
         if self.sort.len() > 0 {
-            let sort = self.sort.first().unwrap();
-            let column = self.get_column_by_name::<E>(&sort.col_id).await?;
+            let sort = &self.sort.first().unwrap();
+            let column= column_map.get(sort.col_id.as_str()).unwrap();
 
-            qs = qs.order_by(column, if sort.sort.to_lowercase() == "asc" { Order::Asc } else { Order::Desc });
+            qs = qs.order_by(column.clone(), if sort.sort.to_lowercase() == "asc" { Order::Asc } else { Order::Desc });
         }
 
         // Paging
@@ -103,13 +87,3 @@ impl Query {
         qs.all(db).await
     }
 }
-
-/* To debug column names
-pub fn print_column_names<E: EntityTrait>() {
-    println!("Noms des colonnes avec leur format exact:");
-    for col in E::Column::iter() {
-        println!("  - Nom exact: '{:?}'", col);
-        // Affiche aussi le nom en tant que string pour voir la différence
-        println!("    Format string: '{}'", format!("{:?}", col));
-    }
-}*/
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,20 @@ mod filter;
 use futures::executor::block_on;
 use sea_orm::{Database, DbErr};
 use entities::{prelude::*};
+use crate::entities::entity;
 use crate::filter::{Query};
 
 const DATABASE_URL: &str = "postgres://rust:rust@localhost:5432/rust";
 
 async fn run() -> Result<(), DbErr> {
     let db = Database::connect(DATABASE_URL).await?;
+    
     let global_searchable = vec!["colonne_1", "colonne_2"];
+    
+    let column_map = HashMap::from([
+        ("colonne_1", entity::Column::Colonne1),
+        ("colonne_2", entity::Column::Colonne2),
+    ]);
 
     let query = Query {
         start: 0,
@@ -21,7 +28,7 @@ async fn run() -> Result<(), DbErr> {
         global_search: "".to_string(),
     };
 
-    let entities = query.apply_filters::<Entity>(&global_searchable, &db).await?;
+    let entities = query.apply_filters::<Entity>(&global_searchable, &column_map, &db).await?;
 
     println!("{:?}", entities);
     println!("Done :)");
